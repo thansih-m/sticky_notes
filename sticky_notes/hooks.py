59bec56236529a44b5451d08782dcf46f3aabf9e
Thansih <thansih@Thansihs-MacBook-M4.local>
2026-02-22 00:52:24 +0400

    Added module fixture for auto module creation

--- sticky_notes/hooks.py
+++ sticky_notes/hooks.py
@@ -247,3 +247,5 @@ app_license = "mit"
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+fixtures = ["Module Def"]
+
